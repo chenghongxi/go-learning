4fe4c605d648f3a10709ec5903ccaaeb95707348
chenghongxi <422459409@qq.com>
2022-09-28 21:15:20 +0800

    learning loadconfig yaml

--- go.mod
+++ go.mod
@@ -10,4 +10,5 @@ require (
 	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ loadconfig/loadfile.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"os"
+
+	"go-learning/loadconfig/config"
+)
+
+func loadConfigFromFile(file string) (*config.PixiuConfiguration, error) {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return loadConfig(data)
+}
+
+func loadConfig(data []byte) (*config.PixiuConfiguration, error) {
+	var pc config.PixiuConfiguration
+	if err := yaml.Unmarshal(data, &pc); err != nil {
+		return nil, err
+	}
+
+	return &pc, nil
+}
+
+func main() {
+	pixiuConfiguration, err := loadConfigFromFile("test.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(pixiuConfiguration)
+}
