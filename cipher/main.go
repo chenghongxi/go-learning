c3c5cf417bc826d076457e2d96639350514396ba
chenghongxi <422459409@qq.com>
2022-10-21 14:36:42 +0800

    Update main.go

--- cipher/main.go
+++ cipher/main.go
@@ -7,16 +7,17 @@ import (
 
 func main() {
 
+	// 定义变量b 类型为：byte数组
 	b := []byte("hello,world")
 
-	// 加密a
+	// 加密b
 	encrypt, err := cipher.Encrypt(b)
 	if err != nil {
 		klog.Error("Encrypt Fail ：", err)
 	}
 	klog.Info("Encrypt suesses:", encrypt)
 
-	// 解密
+	// 对上一步加密变量解密
 	decrypt, err := cipher.Decrypt(encrypt)
 	if err != nil {
 		klog.Error("Decrypt Fail ：", err)
