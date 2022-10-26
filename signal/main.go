51d4ef12a3b0846e9144450ec6164d5d7c16122f
chenghongxi <422459409@qq.com>
2022-10-26 09:39:40 +0800

    learning signal

---
+++ signal/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"k8s.io/klog/v2"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	// 创建监听chan 监听源：系统信号
+	c := make(chan os.Signal)
+	signal.Notify(c,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGKILL,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+
+	go func() {
+		for s := range c {
+			switch s {
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM, syscall.SIGQUIT:
+				klog.Error("shutdown server......")
+				exit()
+
+			default:
+				klog.Error("other signal shutdown server......")
+			}
+		}
+	}()
+
+	klog.Info("starting server......")
+	sum := 0
+	for {
+		sum++
+		klog.Infoln("sum :", sum)
+		time.Sleep(time.Second)
+	}
+}
+
+func exit() {
+	klog.Infoln("Execute Clean...")
+	klog.Infoln("End Exit...")
+	os.Exit(0)
+}
