65ac25943eb0e56cac6eacadb365fdf5bb227a15
chenghongxi <422459409@qq.com>
2022-12-14 09:53:44 +0800

    Create proxy.go

---
+++ proxy/proxy.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	// gin 指定代理，apis 原始请求转发到 k8s apiserver
+	route := gin.Default()
+
+	route.Any("/*proxy", proxyHandler)
+
+	_ = route.Run(":8090")
+
+}
+
+func proxyHandler(c *gin.Context) {
+	
+}
